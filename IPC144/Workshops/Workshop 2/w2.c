396909832181dd7959e8d4e182a7bc0daf30294a
Natalia Monsalve <natalia.monsalve.m@gmail.com>
2019-07-29 21:16:45 -0400

    Create w2.c

---
+++ IPC144/Workshops/Workshop 2/w2.c
@@ -0,0 +1,46 @@
+// Name: Natalia Monsalve
+// w2.c
+
+int main(void)
+{
+  float strength, speed, defense, intelligence = 0;
+  int sum, luck = 0;
+  float strengthRatio, speedRatio, defenseRatio, intelligenceRatio = 0;
+  
+  printf("Character Creation\n");
+  printf("Please enter your desired stats for your character:\n\n");
+  
+  printf("Enter strength: ");
+  scanf("%f", &strength);
+  
+  printf("Enter speed: ");
+  scanf("%f", &speed);
+  
+  printf("Enter defense: ");
+  scanf("%f", %defense);
+  
+  printf("Enter intelligence");
+  scanf("%f", &intelligence);
+  
+  printf("\n");
+  
+  sum = strength + speed + defense + intelligence;
+  
+  strengthRatio = strength / sum * 100;
+  speedRatio = speed / sum * 100;
+  defenseRatio = defense / sum * 100;
+  intelligenceRatio = intelligence / sum * 100;
+  
+  printf("Your player's final states are:\n\n");
+  
+  printf("Strength: %d\n", (int)strengthRatio);
+  printf("Speed: %d\n", (int)speedRatio);
+  printf("Defense: %d\n", (int)defenseRatio);
+  printf("Intelligence: %d\n", (int)intelligenceRatio);
+  
+  luck = sum % 30;
+  
+  printf("Luck: %d\n", luck);
+  
+  return 0;
+}
