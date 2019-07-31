ffa2756fa04e77e1d85bab091a9a6a180d04a40a
Natalia Monsalve <natalia.monsalve.m@gmail.com>
2019-07-31 17:43:14 -0400

    Create w4.c

---
+++ IPC144/Workshops/Workshop 4/w4.c
@@ -0,0 +1,39 @@
+// Name: Natalia Monsalve
+// w4.c
+
+#include <stdio.h>
+
+void clearScreen();
+int validate(int low, int high);
+void newGame();
+void load();
+
+int main(void) 
+{
+  int input;
+  
+  void clearScreen();
+  
+  printf("--Main Menu--\n\n");
+  
+  printf("1 - New Game\n");
+  printf("2 - Load Game\n");
+  printf("3 - Exit\n\n");
+  
+  validate(1, 3);
+  
+  return 0;
+{
+
+void clearScreen()
+{
+  int i;
+  for (i = 0; i <= 40; i++) {
+    printf("\n");
+  }
+}
+
+int validate(int low, int high) {
+  int input;
+  printf("Select: ");
+}
