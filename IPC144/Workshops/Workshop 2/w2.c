c9eef9ed92937a13cfd52e43d8aab8a9d9435baf
Natalia Monsalve <natalia.monsalve.m@gmail.com>
2019-08-01 20:13:10 -0400

    Update w2.c

--- IPC144/Workshops/Workshop 2/w2.c
+++ IPC144/Workshops/Workshop 2/w2.c
@@ -5,9 +5,9 @@
 
 int main(void)
 {
-  float playerStrength, playerSpeed, playerDefense, playerIntelligence = 0;
-  int sum, luck = 0;
+  float playerStrength, playerSpeed, playerDefense, playerIntelligence = 0; 
   float strengthRatio, speedRatio, defenseRatio, intelligenceRatio = 0;
+  int sum, luck = 0;
   
   printf("Character Creation\n");
   printf("Please enter your desired stats for your character:\n\n");
@@ -33,15 +33,14 @@ int main(void)
   defenseRatio = playerDefense / sum * 100;
   intelligenceRatio = playerIntelligence / sum * 100;
   
+  luck = sum % 30;
+  
   printf("Your player's final states are:\n\n");
   
   printf("Strength: %d\n", (int)strengthRatio);
   printf("Speed: %d\n", (int)speedRatio);
   printf("Defense: %d\n", (int)defenseRatio);
   printf("Intelligence: %d\n", (int)intelligenceRatio);
-  
-  luck = sum % 30;
-  
   printf("Luck: %d\n", luck);
   
   return 0;
