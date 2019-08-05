b3daf805b75afd797ab62dcc9437d9009c619af9
Natalia Monsalve <natalia.monsalve.m@gmail.com>
2019-08-05 13:14:37 -0400

    Create project.c

---
+++ IPC144/Project/project.c
@@ -0,0 +1,9 @@
+// Name: Natalia Monsalve
+// project.c
+
+/* I declare that the attached assignment is wholly my own work in accordance with Seneca Academic Policy. No part of this assignment has been copied manually or electronically from any other source (including web sites) or distributed to other students. */
+
+#include <stdio.h>
+#define MAX_INVENTORY_SIZE 10
+#define TAX .13
+#define MAX_ITEMS 10
