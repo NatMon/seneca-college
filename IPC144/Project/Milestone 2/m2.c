6e1007b1e1b456267213c2b9650d4bded2795a68
Natalia Monsalve <natalia.monsalve.m@gmail.com>
2019-08-13 23:02:47 -0400

    Update m2.c

--- IPC144/Project/Milestone 2/m2.c
+++ IPC144/Project/Milestone 2/m2.c
@@ -1,5 +1,5 @@
 // Name: Natalia Monsalve
-// project.c
+// m2.c
 
 /* I declare that the attached assignment is wholly my own work in accordance with Seneca Academic Policy. No part of this assignment has been copied manually or electronically from any other source (including web sites) or distributed to other students. */
 
